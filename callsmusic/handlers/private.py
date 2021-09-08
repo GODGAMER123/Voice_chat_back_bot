c0599b0f2fc7aa2fe965b2d736cf16beb70cf36a
GODGAMER123 <73989136+GODGAMER123@users.noreply.github.com>
2021-09-08 15:44:00 +0530

    Update private.py

--- callsmusic/handlers/private.py
+++ callsmusic/handlers/private.py
@@ -24,10 +24,10 @@ async def start(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        '🔈 Channel', url='https://t.me/callsmusic',
+                        '🔈 OWNER', url='https://t.me/Henil_is_owner',
                     ),
                     InlineKeyboardButton(
-                        'Group 💬', url='https://t.me/callsmusicchat',
+                        'Group 💬', url='https://t.me/moviesbvn',
                     ),
                 ],
             ],
